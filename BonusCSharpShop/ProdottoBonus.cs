33a1266687a9f9f65366fe289c4b9e41e44671bc
lucianocecere07 <cecereluciano97@gmail.com>
2022-11-29 16:15:09 +0100

    BONUS: metodo per codice con un pad left di 0 fino a 8

---
+++ BonusCSharpShop/ProdottoBonus.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//BONUS
+
+namespace BonusCSharpShop
+{
+    public class ProdottoBonus
+    { 
+        //proprietà
+        private int codice;
+        private string nome;
+        private string descrizione;
+        private double prezzo;
+        private int iva;
+
+        //costruttori
+        public ProdottoBonus(int codice, string nome, string descrizione, double prezzo, int iva)
+        {
+            this.codice = codice;
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.iva = iva;
+        }
+
+        //getter
+        public int GetCodice()
+        {
+            return this.codice;
+        }
+
+        public string GetNome()
+        {
+            return this.nome;
+        }
+
+        public string GetDescrizione()
+        {
+            return this.descrizione;
+        }
+
+        public double GetPrezzo()
+        {
+            return this.prezzo;
+        }
+
+        public int GetIva()
+        {
+            return this.iva;
+        }
+        //setter
+        public void SetNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public void SetDescrizione(string descrizione)
+        {
+            this.descrizione = descrizione;
+        }
+
+        public void SetPrezzo(double prezzo)
+        {
+            if (prezzo > 0)
+            {
+                this.prezzo = prezzo;
+            }
+            else
+            {
+                Console.WriteLine("il prezzo deve essere maggiore di 0");
+            }
+        }
+
+        public void SetIva(int iva)
+        {
+            if (iva >= 0)
+            {
+                this.iva = iva;
+            }
+            else
+            {
+                Console.WriteLine("l'iva non può essere negativa");
+            }
+        }
+
+        //metodi
+        public void StampaPrezzo()
+        {
+            //prezzo con 2 decimali
+            Console.WriteLine("Prezzo (senza iva): " + prezzo.ToString("F") + " euro");
+            double prezzoTotale = prezzo / 100 * iva + prezzo;
+            Console.WriteLine("Prezzo (con iva): " + prezzoTotale.ToString("F") + " euro");
+        }
+
+        public void StampaNome()
+        {
+            Console.WriteLine("Nome completo del prodotto: " + codice + nome);
+        }
+
+        public void StampaProdotto()
+        {
+            Console.WriteLine("------------------");
+            Console.WriteLine("nome: " + nome);
+            Console.WriteLine("descrizione: " + descrizione);
+            Console.WriteLine("prezzo(senza iva): " + prezzo.ToString("F") + " euro");
+            Console.WriteLine("iva: " + iva + "%");
+            Console.WriteLine("------------------");
+        }
+        
+        //BONUS
+        public void CodicePadLeft()
+        {
+            string codicePadLeft = Convert.ToString(codice).PadLeft(8, '0');
+            Console.WriteLine("il codice con 8 caratteri: " + codicePadLeft);
+        }
+    }
+}
---
+++ BonusCSharpShop/Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+BONUS:
+create un metodo che restituisca il codice con un pad left di 0 per arrivare a 8 caratteri 
+(ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
+Usando una List, dichiarate una lista dei prodotti di un negozio e inseriteci al suo interno qualche prodotto 
+che vi aspettate di trovare nel negozio. Stampate poi l’elenco dei vostri prodotti che avete previsto nel negozio.
+*/
+
+//BONUS
+
+using BonusCSharpShop;
+
+//generare un numero random da 1 a 100
+Random generatoreNumeriRandom = new Random();
+int numeroRandom = generatoreNumeriRandom.Next(1, 100);
+
+ProdottoBonus prodotto = new ProdottoBonus(numeroRandom, "shampoo", "per capelli normali", 1.60, 22);
+
+//stampa prezzo senza/con iva
+prodotto.StampaPrezzo();
+
+//stampa nome completo del prodotto
+prodotto.StampaNome();
+
+//stampa il prodotto
+prodotto.StampaProdotto();
+
+// codice con un pad left di 0 per arrivare a 8 caratteri
+prodotto.CodicePadLeft();
+
+
+
+
+
+
+
+
+
+
+
+
+
+//---------------------------------------esercizio--------------------------------------------------------//
+
+/*
+Nel progetto CSharpShop , creare la classe Prodotto che gestisce i prodotti del vostro shop.
+Un prodotto è caratterizzato da: codice (numero intero), nome, descrizione, prezzo, iva.
+Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter ed 
+eventuali altri metodi di “utilità” per fare in modo che:
+- alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random.
+- Il codice prodotto sia accessibile solo in lettura
+- Gli altri attributi siano accessibili sia in lettura che in scrittura
+Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva.
+Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
+Testate poi i vostri oggetti Prodotto, istanziandoli e provando ad interagirci con loro per testare tutti i metodi 
+che avete previsto e implementato.
+
+
+using CSharpShop;
+
+//generare un numero random da 1 a 100
+Random generatoreNumeriRandom = new Random();
+int numeroRandom = generatoreNumeriRandom.Next(1, 100);
+
+//creazione nuovo prodotto
+Prodotto prodottoNegozio = new Prodotto(numeroRandom, "shampoo", "per capelli normali", 1.60, 22);
+
+//stampa prezzo senza/con iva
+prodottoNegozio.StampaPrezzo();
+
+//stampa nome completo del prodotto
+prodottoNegozio.StampaNome();
+*/
+
+//----------------------------------------------------------------------------------------------------------//
