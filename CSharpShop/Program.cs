50237d45b42f207e9fd9b2c0e310140de5174b72
lucianocecere07 <cecereluciano97@gmail.com>
2022-11-29 14:04:52 +0100

    creazione classe Prodotto

--- CSharpShop/Program.cs
+++ CSharpShop/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+/*
+Nel progetto CSharpShop , creare la classe Prodotto che gestisce i prodotti del vostro shop.
+Un prodotto è caratterizzato da: codice (numero intero), nome, descrizione, prezzo, iva.
+Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter ed 
+eventuali altri metodi di “utilità” per fare in modo che:
+- alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random.
+- Il codice prodotto sia accessibile solo in lettura
+- Gli altri attributi siano accessibili sia in lettura che in scrittura
+Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva.
+Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
+Testate poi i vostri oggetti Prodotto, istanziandoli e provando ad interagirci con loro per testare tutti i metodi 
+che avete previsto e implementato.
+*/
 
